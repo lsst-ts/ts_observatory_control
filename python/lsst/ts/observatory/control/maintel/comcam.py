a94ecdf3108df1f774e3dd7dbfcac8c906be04f5
Tiago Ribeiro <tribeiro@lsst.org>
2022-01-19 12:33:15 -0700

    In `ComCam`: - Remove specialized implementation of the `expose` method. - Add new abstract property, `camera`. - Add new abstract method `parse_sensors`

--- python/lsst/ts/observatory/control/maintel/comcam.py
+++ python/lsst/ts/observatory/control/maintel/comcam.py
@@ -135,71 +135,25 @@ class ComCam(BaseCamera):
             **kwargs,
         )
 
-    async def expose(
-        self,
-        exp_time,
-        shutter,
-        image_type,
-        group_id,
-        test_type=None,
-        sensors=None,
-        note=None,
-    ):
-        """Encapsulates the take image command.
+    @property
+    def camera(self):
+        """Camera remote."""
+        return self.rem.cccamera
 
-        This basically consists of configuring and sending a takeImages
-        command to the camera and waiting for an endReadout event.
+    def parse_sensors(self, sensors):
+        """Parse input sensors.
 
         Parameters
         ----------
-        exp_time : `float`
-            The exposure time for the image, in seconds.
-        shutter : `bool`
-            Should activate the shutter? (False for bias and dark)
-        image_type : `str`
-            Image type (a.k.a. IMGTYPE) (e.g. e.g. BIAS, DARK, FLAT, FE55,
-            XTALK, CCOB, SPOT...)
-        group_id : `str`
-            Image groupId. Used to fill in FITS GROUPID header
-        test_type : `str`
-            The classifier for the testing type. Usually the same as
-            `image_type`.
         sensors : `str`
             A colon delimited list of sensor names to use for the image.
-        note : `str`
-            A freeform string containing small notes about the image.
 
         Returns
         -------
-        endReadout : ``self.cccamera.evt_endReadout.DataType``
-            End readout event data.
+        sensors : `str`
+            A valid set of sensors.
         """
-        async with self.cmd_lock:
-            # FIXME: Current version of CCCamera software is not set up to take
-            # images with numImages > 1, so this is fixed at 1 for now and we
-            # loop through any set of images we want to take.
-
-            key_value_map = (
-                f"imageType: {image_type}, groupId: {group_id}, "
-                f"testType: {image_type if test_type is None else test_type}"
-            )
-
-            self.rem.cccamera.cmd_takeImages.set(
-                numImages=1,
-                expTime=float(exp_time),
-                shutter=bool(shutter),
-                keyValueMap=key_value_map,
-                sensors="" if sensors is None else sensors,
-                obsNote="" if note is None else note,
-            )
-
-            timeout = self.read_out_time + self.long_timeout + self.long_long_timeout
-            self.rem.cccamera.evt_endReadout.flush()
-            await self.rem.cccamera.cmd_takeImages.start(timeout=timeout + exp_time)
-            end_readout = await self.rem.cccamera.evt_endReadout.next(
-                flush=False, timeout=timeout
-            )
-            return end_readout
+        return "" if sensors is None else sensors
 
     async def setup_instrument(self, **kwargs):
         """Implements abstract method to setup instrument.
