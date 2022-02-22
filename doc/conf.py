c818b8cbd5aae37150ca3ff6d0d1c6c190dc2e9f
Russell Owen <rowen@uw.edu>
2022-02-22 13:34:29 -0800

    doc/conf.py: make linters happier

--- doc/conf.py
+++ doc/conf.py
@@ -2,14 +2,14 @@
 This configuration only affects single-package Sphinx documentation builds.
 """
 
-from documenteer.conf.pipelinespkg import *
-import lsst.ts.observatory.control
+from documenteer.conf.pipelinespkg import *  # noqa
+import lsst.ts.observatory.control  # noqa
 
 project = "ts_observatory_control"
-html_theme_options["logotext"] = project
+html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
 doxylink = {}  # Avoid warning: Could not find tag file _doxygen/doxygen.tag
 
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)
-intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa
