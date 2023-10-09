00b28c13f13149662939770ae696efb44e126727
edennihy <edennihy@lsst.org>
2023-10-09 11:21:34 -0700

    In constants/latiss_constants.py, update blue300lpmm_qn1 sweet spot for new mask.

--- python/lsst/ts/observatory/control/constants/latiss_constants.py
+++ python/lsst/ts/observatory/control/constants/latiss_constants.py
@@ -29,7 +29,7 @@ sweet_spots = {  # the sweet spots for the gratings (pixels)
     "ronchi90lpmm": PointD(1780, 1800),
     "ronchi170lpmm": PointD(1750, 300),  # estimate added in DM-28818
     "holo4_003": PointD(1750, 300),  # estimate added in DM-28818
-    "blue300lpmm_qn1": PointD(1750, 300),
+    "blue300lpmm_qn1": PointD(1750, 200),
     "empty_1": PointD(1780, 1800),
     "holo4_001": PointD(1750, 300),  # based on holo4_003
 }
