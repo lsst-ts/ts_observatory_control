45bbe1a92821f2746f40e71ec474660cd81b3594
Parker Fagrelius <parfa30@gmail.com>
2025-04-18 13:34:42 -0400

    position0 ->position[0]

--- python/lsst/ts/observatory/control/maintel/mtcalsys.py
+++ python/lsst/ts/observatory/control/maintel/mtcalsys.py
@@ -441,7 +441,7 @@ class MTCalsys(BaseCalsys):
 
         for location, value in self.linearstage_projector_locations.items():
             if (
-                abs(float(select_location.position0) - float(value))
+                abs(float(select_location.position[0]) - float(value))
                 < self.linearstage_projector_pos_tolerance
             ):
                 projector_location = location
@@ -450,17 +450,18 @@ class MTCalsys(BaseCalsys):
 
         self.log.info(
             f"Projector Location is {projector_location}, \n"
-            f"LED Location stage pos @: {led_location.position0}, \n"
-            f"LED Focus stage pos @: Not working, \n"  # {led_focus.position0}
-            f"Laser Focus stage pos @: {laser_focus.position0}, \n"
+            f"LED Location stage pos @: {led_location.position[0]}], \n"
+            f"LED Focus stage pos @: Not working, \n"
+            # {led_focus.position[0]}
+            f"Laser Focus stage pos @: {laser_focus.position[0]}, \n"
             f"LED State stage pos @: {led_state}"
         )
 
         return (
             projector_location,
-            float(led_location.position0),
-            0.0,  # float(led_focus.position0),
-            float(laser_focus.position0),
+            float(led_location.position[0]),
+            0.0,  # float(led_focus.position[0]),
+            float(laser_focus.position[0]),
             str(led_state),
         )
 
@@ -574,7 +575,7 @@ class MTCalsys(BaseCalsys):
             vertical_pos = await self.linearstage_projector_select.tel_position.next(
                 flush=True, timeout=self.long_timeout
             )
-            if vertical_pos.position0 != self.linearstage_projector_locations["led"]:
+            if vertical_pos.position[0] != self.linearstage_projector_locations["led"]:
                 self.log.info("Projector select stage is not aligned with LED position")
                 self.linearstage_projector_select.cmd_moveAbsolute.set_start(
                     distance=self.linearstage_projector_locations["led"],
