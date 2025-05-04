3f7c96796a2ddf47244124f531bfae9b5d420bea
elanaku <eurbach@g.harvard.edu>
2025-05-04 14:43:55 -0400

    Fix metadata

--- python/lsst/ts/observatory/control/maintel/mtcalsys.py
+++ python/lsst/ts/observatory/control/maintel/mtcalsys.py
@@ -1055,7 +1055,6 @@ class MTCalsys(BaseCalsys):
                 mtcamera_exptime,
                 nflats=1,
                 filter=mtcamera_filter,
-                note=f"{sequence_name}_{wavelength}_laser_{laser_mode}",
                 **exposure_metadata,
             )
         else:
