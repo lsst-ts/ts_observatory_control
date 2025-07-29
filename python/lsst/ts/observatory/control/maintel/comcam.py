e64ca89158fe5d8b1b80aad1dfc4bb1f25a6bb1a
Marco Rocchietti <marco.rocchietti@unc.edu.ar>
2025-07-29 18:04:00 -0300

    Refactor BaseCamera.valid_imagetype attribute into the class method BaseCamera.get_image_types

--- python/lsst/ts/observatory/control/maintel/comcam.py
+++ python/lsst/ts/observatory/control/maintel/comcam.py
@@ -97,9 +97,11 @@ class ComCam(BaseCamera):
         self.shutter_time = 1  # time to open or close shutter (sec)
         self.filter_change_timeout = 60  # time for filter to get into position (sec)
 
-        self.valid_imagetype.append("SPOT")
-
         self.cmd_lock = asyncio.Lock()
+
+    @classmethod
+    def get_image_types(cls) -> typing.List[str]:
+        return super().get_image_types() + ["SPOT"]
 
     async def take_spot(
         self,
