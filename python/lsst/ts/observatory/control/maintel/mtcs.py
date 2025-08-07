dfed3e8fcd0a488fe917a2d79c78a75245cdcfec
Tiago Ribeiro <tribeiro@lsst.org>
2025-08-07 00:33:18 -0700

    In maintel/mtcs.py, in ``MTCS._ready_to_take_data`` use ``wait_for_in_position`` instead of the custom implementation that was only taking into account the mount and the camera hexapod.

--- python/lsst/ts/observatory/control/maintel/mtcs.py
+++ python/lsst/ts/observatory/control/maintel/mtcs.py
@@ -3239,16 +3239,7 @@ class MTCS(BaseTCS):
         # TODO: Finish implementation.
 
         try:
-            await asyncio.gather(
-                self.wait_for_mtmount_inposition(self.long_timeout, False),
-                self._handle_in_position(
-                    in_position_event=self.rem.mthexapod_1.evt_inPosition,
-                    timeout=self.long_timeout,
-                    settle_time=0.0,
-                    component_name="Camera Hexapod",
-                    race_condition_timeout=self.hexapod_ready_to_take_data_timeout,
-                ),
-            )
+            await self.wait_for_inposition(timeout=self.long_timeout)
         except asyncio.TimeoutError:
             self.log.warning("Mount, Camera Hexapod or Rotator not in position.")
 
