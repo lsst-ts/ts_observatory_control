f182dcf15d5e3a77e9c6613dcaa37e3844b1b1e0
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2026-08-26 10:43:51 -0700

    Add new header license pre-commit hook

--- python/lsst/ts/observatory/control/data/scripts/guider_roi/guider_roi_test_script.py
+++ python/lsst/ts/observatory/control/data/scripts/guider_roi/guider_roi_test_script.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# This file is part of ts_observatory_control.
+
+
+# This file is part of ts-observatory-control.
 #
-# Developed for the Vera Rubin Observatory Telescope and Site.
+# Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 # This product includes software developed by the LSST Project
 # (https://www.lsst.org).
 # See the COPYRIGHT file at the top-level directory of this distribution
@@ -14,12 +16,11 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
 Simple test script for GuiderROIs with existing Butler repository.
