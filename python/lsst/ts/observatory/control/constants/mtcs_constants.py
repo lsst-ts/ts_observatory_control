b48549f0e740630493687212d0aebc3e5dbd05c1
edennihy <edennihy@lsst.org>
2026-09-24 14:37:51 -0700

    Add new SimonyiVentParameters constant class for use with maintel/prepare_for/vent.py script.

--- python/lsst/ts/observatory/control/constants/mtcs_constants.py
+++ python/lsst/ts/observatory/control/constants/mtcs_constants.py
@@ -19,4 +19,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+import dataclasses
+
 plate_scale = 0.05  # mm/arcsecond at the telescope focal plane
+
+
+@dataclasses.dataclass(frozen=True)
+class SimonyiVentParameters:
+    """Named, immutable parameters for the Simonyi (Main) Telescope's
+    evening venting protocol.
+
+    Attributes
+    ----------
+    sun_elevation_high : `float`
+        Sun elevation (deg) above which the dome shutter and louvers stay
+        fully closed; the dome is only nudged for sun-avoidance.
+    sun_elevation_horizon : `float`
+        Sun elevation (deg) at/below which the sun-avoidance constraint no
+        longer applies and the dome/telescope reposition for the wind.
+    sun_elevation_stop : `float`
+        Sun elevation (deg) at which venting proceeds regardless of the
+        temperature condition.
+    tel_vent_elevation : `float`
+        Telescope elevation (deg) used while venting.
+    louver_sun_avoidance_angle : `float`
+        Minimum angular separation (deg) a louver must keep from the sun.
+    louver_sun_exposed_percent : `float`
+        Percent-open cap for louvers within the sun avoidance angle.
+    temperature_differential_threshold : `float`
+        Outside-minus-indoor temperature threshold (deg C) that must be met
+        before venting starts.
+    loop_wait_time : `float`
+        Seconds to wait between iterations of the venting polling loops.
+    dome_min_az : `float`
+        Minimum dome azimuth (deg) allowed while the sun is up.
+    dome_max_az : `float`
+        Maximum dome azimuth (deg) allowed while the sun is up.
+    """
+
+    sun_elevation_high: float = 6.0
+    sun_elevation_horizon: float = -1.0
+    sun_elevation_stop: float = -6.0
+    tel_vent_elevation: float = 30.0
+    louver_sun_avoidance_angle: float = 60.0
+    louver_sun_exposed_percent: float = 50.0
+    temperature_differential_threshold: float = -1.0
+    loop_wait_time: float = 30.0
+    dome_min_az: float = 30.0
+    dome_max_az: float = 150.0
